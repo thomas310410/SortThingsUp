e0afc3b6028f6fff91da33069da791bc9106b1d1
Kodai <102161240+AymenKodai@users.noreply.github.com>
2024-02-04 16:44:18 +0100

    Grid Test

---
+++ Assets/Scripts/GAME_MANAGER.cs
@@ -0,0 +1,41 @@
+using Hypertonic.GridPlacement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GAME_MANAGER : MonoBehaviour
+{
+    [SerializeField]
+    private GridSettings gridSettings;
+
+    [SerializeField]
+    private GameObject gridObjectPrefab;
+
+    private GridManager gridManager;
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 150, 50), "Comfirm Placement"))
+            ConfirmPlacement();
+
+        if (GUI.Button(new Rect(10, 10, 150, 50), "Enter"))
+            EnterPlacementMode();
+
+    }
+    private void start()
+    {
+        gridManager = new GameObject("Grid Manager").AddComponent<GridManager>();
+        gridManager.Setup(gridSettings);
+    }
+
+    private void EnterPlacementMode()
+    {
+        GameObject gridObject = Instantiate(gridObjectPrefab);
+        gridManager.EnterPlacementMode(gridObject);
+    }
+
+    private void ConfirmPlacement()
+    {
+        gridManager.ConfirmPlacement();
+    }
+}
